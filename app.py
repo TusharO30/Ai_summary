355ad656b318e5a258538ec62fca1626bb8fbb7d
TusharO30 <jw251309@gmail.com>
2025-04-07 23:25:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ CORS(app)
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 if not GOOGLE_API_KEY:
     raise ValueError("GOOGLE_API_KEY environment variable not set")
-
+    
+@app.route("/")
+def home():
+    return render_template("index")
+    
+@app.route("/check-auth")
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-pro')
 @app.route("/check-auth")
